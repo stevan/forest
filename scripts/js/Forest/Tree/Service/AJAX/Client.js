a367c828231165c5d5b3796779b9a9612bf37808
Stevan Little <stevan.little@iinteractive.com>
2006-09-19 12:35:11 +0000

    twekin

--- scripts/js/Forest/Tree/Service/AJAX/Client.js
+++ scripts/js/Forest/Tree/Service/AJAX/Client.js
@@ -3,7 +3,9 @@ namespace('Forest.Tree.Service.AJAX');
 
 Forest.Tree.Service.AJAX.Client = function (base_url) {
     this.base_url = base_url || 'http://localhost:8080/';
+
     this.request  = false;
+    this.locked   = false;    
     
     // set up the self referant
     this.oid = this.create_oid();
@@ -11,6 +13,33 @@ Forest.Tree.Service.AJAX.Client = function (base_url) {
 };
 
 Forest.Tree.Service.AJAX.Client.prototype = new Forest.Object ();
+
+// Utilitiy Methods
+
+Forest.Tree.Service.AJAX.Client.prototype.get_XMLHTTP_request = function () {
+	var request = false;
+    // branch for native XMLHttpRequest object
+    if (window.XMLHttpRequest) {
+    	try {
+			request = new XMLHttpRequest();
+        } catch(e) {
+			request = false;
+        }
+    // branch for IE/Windows ActiveX version
+    } 
+    else if (window.ActiveXObject) {
+       	try {
+        	request = new ActiveXObject("Msxml2.XMLHTTP");
+      	} catch(e) {
+        	try {
+          		request = new ActiveXObject("Microsoft.XMLHTTP");
+        	} catch(e) {
+          		request = false;
+        	}
+		}    
+    }
+    return request;
+}
 
 Forest.Tree.Service.AJAX.Client.prototype.parse_JSON = function (string) {
     try {
@@ -22,28 +51,59 @@ Forest.Tree.Service.AJAX.Client.prototype.parse_JSON = function (string) {
     }
 };
 
+// API Methods
+
 Forest.Tree.Service.AJAX.Client.prototype.load_tree = function (tree_id) {
 
     var node = document.getElementById(tree_id);
     
     if (node.hasChildNodes()) {
         if (node.style.display == 'none') {
-            node.style.display = 'block';            
+            this.show_tree(node);           
         }
         else {
-            node.style.display = 'none';
+            this.hide_tree(node);
         }
     }
     else {
-        this.request = new XMLHttpRequest();
+        if (this.locked == false) {
+            this.request = this.get_XMLHTTP_request();
     
-        var self = this;
-        this.request.onreadystatechange = function () { self.check_state() };
+            var self = this;
+            this.request.onreadystatechange = function () { self.check_state() };
             
-        this.request.open("GET", (this.base_url + '?tree_id=' + tree_id), true);
-        this.request.send("");    
+            this.request.open("GET", (this.base_url + '?tree_id=' + tree_id), true);
+            this.request.send(""); 
+            this.locked = true;              
+        }   
     }
 }
+
+// Overrideable Methods
+
+Forest.Tree.Service.AJAX.Client.prototype.show_tree = function (node) {
+    node.style.display = 'block';    
+}
+
+Forest.Tree.Service.AJAX.Client.prototype.hide_tree = function (node) {
+    node.style.display = 'none';
+}
+
+Forest.Tree.Service.AJAX.Client.prototype.create_html_for_leaf = function (tree) {
+    return "<li>" + tree.node + "</li>";
+}
+
+Forest.Tree.Service.AJAX.Client.prototype.create_html_for_branch = function (tree) {
+    return "<li><a href=\"javascript:void(0);\" onclick=\"" + this.oid + ".load_tree('" + 
+            tree.uid + 
+            "')\">" + 
+            tree.node + 
+            "</a></li><ul id='" +
+            tree.uid + 
+            "'></ul>";
+}
+
+// Internal Methods
 
 Forest.Tree.Service.AJAX.Client.prototype.check_state = function () {
     if (this.request.readyState == 4) {
@@ -60,6 +120,7 @@ Forest.Tree.Service.AJAX.Client.prototype.check_state = function () {
         else {
             alert("There was a problem retrieving the tree:\n" + this.request.statusText);
         }
+        this.locked = false;
     }
 }
 
@@ -71,16 +132,10 @@ Forest.Tree.Service.AJAX.Client.prototype.insert_trees = function (tree) {
     for (var i = 0; i < tree.children.length; i++) {
         var child = tree.children[i];
         if (child.is_leaf == 1) {
-            HTML += "<li>" + child.node + "</li>";            
+            HTML += this.create_html_for_leaf(child);            
         }
         else {
-            HTML += "<li><a href=\"javascript:void(0);\" onclick=\"" + this.oid + ".load_tree('" + 
-                    child.uid + 
-                    "')\">" + 
-                    child.node + 
-                    "</a></li><ul id='" +
-                    child.uid + 
-                    "'></ul>";        
+            HTML += this.create_html_for_branch(child);        
         }
     }
     
